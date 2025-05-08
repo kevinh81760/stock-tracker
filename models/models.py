4280df1820a63dabfdf13895ee01d1ddd3739f85
kevinh81760 <kevinh81760@gmail.com>
2025-05-08 14:59:59 -0700

    feat: database and model setup

--- models/models.py
+++ models/models.py
@@ -1,4 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
+
+class SearchHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ticker = db.Column(db.String(10), nullable=False)
+    timestamp = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+
+class CachedStockData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    company_json = db.Column(db.Text)
+    stock_json = db.Column(db.Text)
+    last_update = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
